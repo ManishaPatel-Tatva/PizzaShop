02ded7509141ce0acaaebd12adfa279937ae4762
ManishaPatel-Tatva <tatva.pci47@outlook.com>
2025-02-14 19:01:42 +0530

    Email and password functionality

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,48 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PizzaShop.Models;
+using PizzaShop.ViewModel;
 
 namespace PizzaShop.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly PizzashopContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, PizzashopContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
-    public IActionResult Index()
-        {
-            return View();
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel model)
+    {
+        
+
+        if(ModelState.IsValid){
+            var users = await _context.Users.Where(u => u.Email == model.Email).Select(x=> new{x.Email, x.Password}).FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+
+            if(users != null && users.Password == model.Password){
+                
+                return RedirectToAction("Privacy");
+
+
+            }
+        
         }
+        return View(model);
+
+    }
+
+
         
 
     public IActionResult Privacy()
